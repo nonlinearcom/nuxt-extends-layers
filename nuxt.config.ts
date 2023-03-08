dd530d733d7f64a99f80c1ef3fcb2494eb72056e
Manuel Ehrenfeld <manuel@non-linear.com>
2023-03-08 22:04:10 +0100

    packages update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,3 @@
 export default defineNuxtConfig({
   modules: ['@vueuse/nuxt']
-  // postcss: {
-  //   plugins: {
-  //     'postcss-nested': {},
-  //     autoprefixer: {},
-  //     cssnano: {},
-  //   },
-  // },
 })

b144af93c2d1973d9debbf4e62936e706f1cf283
Manuel Ehrenfeld <manuel@non-linear.com>
2023-03-08 22:49:59 +0100

    extends with modules fix

--- components/TestButton.vue
+++ components/TestButton.vue
@@ -4,9 +4,9 @@
 <script setup lang="ts">
 // broken: automatic import
 // broken: import { useDark, useToggle } from '@vueuse/core';
-// broken:
-import { useDark, useToggle } from '../node_modules/@vueuse/core';
-// works: import { useDark, useToggle } from '@/node_modules/@vueuse/core';
+// broken: import { useDark, useToggle } from '../node_modules/@vueuse/core';
+// works:
+import { useDark, useToggle } from '@/node_modules/@vueuse/core';
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
 </script>
